0bac6d37adcd77b28b9830dcfb9d4f72b7b78e61
AnthonyLima <a.lima.quispe@isur.edu.pe>
2015-10-28 18:10:37 -0500

    sexto commit, se agrego guardado de categorias

--- unidades/guardar.php
+++ unidades/guardar.php
@@ -2,14 +2,14 @@
 
 // si no proviene del formulario, redireccionar
 if (sizeof($_POST) == 0) {
-    header('Location: index.php?route=unidades');
+    header('Location: index.php?route=categorias');
 }
 
 $codigo = $_POST['id_unidad'];
 $nombre = $_POST['nombre'];
 
 $f = fopen("data/unidades.csv", "a");
-fwrite($f, $codigo . ", " . $nombre . "\n");
+fwrite($f,rand(1,10) . $codigo . ", " . $nombre . "\n");
 fclose($f);
 
-header('Location: index.php?route=unidades');
+header('Location: index.php?route=categorias');
